4fe8beb7de602998e1e4a1f35c62d5f7a99efbdc
Hervé guerre <herveguerre@hotmail.fr>
2023-05-23 14:07:17 +0200

    maj

--- script/script.js
+++ script/script.js
@@ -11,6 +11,53 @@ function changerFichierCSS() {
   
 }
 
+// fonctions pour que le style soit charger dans l'url 
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function changerFichierCSS() {
+  var lienCSS = document.getElementById('fichierCSS');
+
+  if (lienCSS.getAttribute('href') === 'css/style.css') {
+    lienCSS.setAttribute('href', 'css/style2.css');
+    setCookie('style', 'style2.css', 7); // Stocker le nom du fichier CSS dans le cookie pendant 7 jours
+  } else {
+    lienCSS.setAttribute('href', 'css/style.css');
+    setCookie('style', 'style.css', 7); // Stocker le nom du fichier CSS dans le cookie pendant 7 jours
+  }
+}
+
+// Au chargement de la page, vérifier si un style est stocké dans le cookie et l'appliquer
+window.addEventListener('load', function() {
+  var styleCookie = getCookie('style');
+  var lienCSS = document.getElementById('fichierCSS');
+
+  if (styleCookie) {
+    lienCSS.setAttribute('href', 'css/' + styleCookie);
+  }
+});
+
+
 //burger 
 function toggleNavbar() {
   var navbarLinks = document.getElementById("navbar-links");
